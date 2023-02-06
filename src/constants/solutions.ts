97f10744bba6269d55bf7f174e432992f3e62b17
djyarnell0 <70770731+djyarnell0@users.noreply.github.com>
2023-02-06 16:00:11 -0600

    Added 4 test solutions / reference syntax (#17)
    * Added 4 test solutions / reference syntax
    
    * Add inline type for solutions array objects
    
    ---------
    
    Co-authored-by: Kenan Casey <kenancasey@gmail.com>

---
+++ src/constants/solutions.ts
@@ -0,0 +1,30 @@
+export const SOLUTIONS: {
+  reference: string
+  verseText: string
+  referenceURL: string
+}[] = [
+  {
+    reference: '1JOHN1:1',
+    verseText:
+      'That which was from the beginning, which we have heard, which we have seen with our eyes, which we have looked upon, and our hands have handled, of the Word of life; ',
+    referenceURL: 'https://www.bible.com/bible/1/1JN.1.KJV',
+  },
+  {
+    reference: 'SONGOFSOLOMON8:13',
+    verseText:
+      'Thou that dwellest in the gardens,The companions hearken to thy voice: Cause me to hear it. ',
+    referenceURL: 'https://www.bible.com/bible/1/SNG.8.KJV',
+  },
+  {
+    reference: 'JUDE3',
+    verseText:
+      'Beloved, when I gave all diligence to write unto you of the common salvation, it was needful for me to write unto you, and exhort you that ye should earnestly contend for the faith which was once delivered unto the saints. ',
+    referenceURL: 'https://www.bible.com/bible/1/JUD.1.KJV',
+  },
+  {
+    reference: 'JOHN3:16',
+    verseText:
+      'For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.',
+    referenceURL: 'https://www.bible.com/bible/1/JHN.3.KJV',
+  },
+]
