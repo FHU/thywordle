21e04d8b8e670b9b777ccbcb14cace1099794bb2
Kaden King <70766937+kadenbking@users.noreply.github.com>
2023-02-16 15:48:54 -0600

    THY-28 About Page (#20)
    * created pages, separated app logic
    
    * added about page content
    
    * removed h:auto
    
    * fixed darkMode text, enabled archived games
    
    * updated info icon to question mark
    
    * mobile layout fixes
    
    * improved nav styling and added hover animations
    
    * Replace Lorem ipsum; Add scale animation;
    
    Bump packages (npm audit)
    
    * Update About.tsx
    
    * Update About.tsx
    
    ---------
    
    Co-authored-by: Kenan Casey <kenancasey@gmail.com>

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,8 +2,9 @@ import {
   CalendarIcon,
   ChartBarIcon,
   CogIcon,
-  InformationCircleIcon,
+  QuestionMarkCircleIcon,
 } from '@heroicons/react/outline'
+import { Link } from 'react-router-dom'
 
 import { ENABLE_ARCHIVED_GAMES } from '../../constants/settings'
 import BellTowerLogo from '../logo/BellTowerLogo'
@@ -22,18 +23,28 @@ export const Navbar = ({
   setIsDatePickerModalOpen,
   setIsSettingsModalOpen,
 }: Props) => {
+  const transitionHoverClasses =
+    'origin-center transition delay-100 duration-100 ease-in-out'
+
   return (
     <div className="navbar">
-      <div className="navbar-content px-5 short:h-auto">
-        <div className="flex justify-start md:w-1/4">
-          <BellTowerLogo className="-mt-1 h-10 w-auto fill-black dark:fill-white md:h-11" />
-        </div>
-        <div className="flex grow justify-start md:w-1/2 md:justify-center">
-          <ThyWordleLogo className="-ml-8 mt-2 h-32 w-auto fill-black dark:fill-white md:mx-0 md:h-56" />
-        </div>
+      <div className="navbar-content px-5">
+        <Link to="/about" className="flex justify-start py-4 pr-6 md:w-1/4">
+          <BellTowerLogo
+            className={`${transitionHoverClasses} -mt-1 h-8 w-auto fill-black hover:scale-125 dark:fill-white md:h-11`}
+          />
+        </Link>
+        <Link
+          to="/"
+          className="flex grow justify-start md:w-1/2 md:justify-center"
+        >
+          <ThyWordleLogo
+            className={`${transitionHoverClasses} mt-2 -ml-6 h-auto w-40 fill-black hover:scale-110 dark:fill-white md:mx-0 md:w-72`}
+          />
+        </Link>
         <div className="right-icons space-around mt-2 w-1/2 justify-end md:w-1/4">
           <div className="mr-4 flex flex-row">
-            <InformationCircleIcon
+            <QuestionMarkCircleIcon
               className="h-5 w-5 cursor-pointer dark:stroke-white md:h-7 md:w-7"
               onClick={() => setIsInfoModalOpen(true)}
             />
---
+++ src/pages/About.tsx
@@ -0,0 +1,96 @@
+/* eslint-disable react/jsx-no-target-blank */
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import BellTowerLogo from './../components/logo/BellTowerLogo'
+import ThyWordleLogo from './../components/logo/ThyWordleLogo'
+import favicon from './../img/favicon.png'
+import gitLogoWhite from './../img/github-mark-white.png'
+import gitLogo from './../img/github-mark.png'
+
+function About() {
+  return (
+    <div className="grid w-full grid-cols-8 gap-4">
+      <div className="col-span-6 col-start-2 mt-2 rounded-xl bg-gray-100 text-center dark:bg-slate-800">
+        <img
+          src={favicon}
+          alt="ThyWordle Favicon"
+          className="mx-auto my-12 w-48"
+        />
+        <h1 className="text-l font-bold dark:text-white sm:text-xl md:text-3xl">
+          About
+        </h1>
+        <ThyWordleLogo className="mx-auto -mt-12 h-auto w-64 fill-black dark:fill-white sm:w-80 md:-mt-16 md:w-96" />
+        <p className="mx-auto -mt-10 w-4/5 text-base dark:text-white md:text-xl">
+          ThyWordle is a clone of the popular word game but tweaked to use
+          solutions that are Bible references. The game was designed and
+          developed by Computer Science students at Freed-Hardeman Unviersity as
+          part of their senior capstone course.
+        </p>
+        <div className="my-12 ">
+          <Link
+            to="/"
+            className="text-ll inline-block rounded-lg bg-black p-4 text-center font-bold uppercase text-white transition-all hover:scale-105 dark:bg-white dark:text-slate-900"
+          >
+            Play the Game
+          </Link>
+        </div>
+      </div>
+      <div className="col-span-6 col-start-2 rounded-xl bg-gray-100 dark:bg-slate-800 lg:col-span-3 lg:col-start-2 lg:col-end-5">
+        <BellTowerLogo className="mx-auto my-8 h-auto w-24 fill-black dark:fill-white md:w-32" />
+        <h2 className="text-center text-2xl font-bold text-black dark:text-white">
+          Learn More <br className="block md:hidden" /> about FHU
+        </h2>
+        <div className="my-12 flex flex-col text-center">
+          <a
+            href="https://fhu.edu"
+            target="_blank"
+            className="text-l mx-auto my-2 w-64 rounded-lg bg-black p-4 text-center font-bold uppercase text-white transition-all hover:scale-105 dark:bg-white dark:text-slate-900"
+          >
+            FHU
+          </a>
+          <a
+            href="https://fhu.edu/cs"
+            target="_blank"
+            className="text-l my-2 mx-auto w-64 rounded-lg bg-black p-4 text-center font-bold uppercase text-white transition-all hover:scale-105 dark:bg-white dark:text-slate-900"
+          >
+            FHU Computer Science
+          </a>
+        </div>
+      </div>
+      <div className="col-span-6 col-start-2 rounded-xl bg-gray-100 dark:bg-slate-800 lg:col-span-3 lg:col-end-8">
+        <img
+          src={gitLogo}
+          alt="GitHub"
+          className="mx-auto mt-20 mb-10 block h-auto w-24 dark:hidden md:w-32 lg:mb-16"
+        />
+        <img
+          src={gitLogoWhite}
+          alt="GitHub"
+          className="mx-auto mt-20 mb-10 hidden h-auto w-24 dark:block md:w-32 lg:mb-16"
+        />
+        <h2 className="text-center text-2xl font-bold text-black dark:text-white">
+          Learn More <br className="block md:hidden" /> about the code
+        </h2>
+        <div className="my-12 flex flex-col text-center">
+          <a
+            href="https://github.com/FHU/thywordle"
+            target="_blank"
+            className="text-l mx-auto my-2 w-64 rounded-lg bg-black p-4 text-center font-bold uppercase text-white transition-all hover:scale-105 dark:bg-white dark:text-slate-900"
+          >
+            ThyWordle Repo
+          </a>
+          <a
+            href="https://github.com/cwackerfuss/react-wordle"
+            target="_blank"
+            className="text-l mx-auto my-2 w-64 rounded-lg bg-black p-4 text-center font-bold uppercase text-white transition-all hover:scale-105 dark:bg-white dark:text-slate-900"
+          >
+            Reactle Repo
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
