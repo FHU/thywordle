76037a0dda3e57fa4622455a5228b06fffc9df16
Kenan Casey <kenancasey@gmail.com>
2023-02-16 15:41:59 -0600

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -23,7 +23,7 @@ function About() {
         <ThyWordleLogo className="mx-auto -mt-12 h-auto w-64 fill-black dark:fill-white sm:w-80 md:-mt-16 md:w-96" />
         <p className="mx-auto -mt-10 w-4/5 text-base dark:text-white md:text-xl">
           ThyWordle is a clone of the popular word game but tweaked to use
-          solutions that are Bible references. Our game was designed and
+          solutions that are Bible references. The game was designed and
           developed by Computer Science students at Freed-Hardeman Unviersity as
           part of their senior capstone course.
         </p>
