dfebaa0982e61ab2ac0c79c45f81b24ff188f1a2
djyarnell0 <djyarnell@hotmail.com>
2023-02-19 23:45:50 -0600

    Added text styling

--- src/components/modals/HintModal.tsx
+++ src/components/modals/HintModal.tsx
@@ -13,22 +13,20 @@ type Props = {
 export const HintModal = ({ isOpen, handleClose, verseText }: Props) => {
   return (
     <BaseModal title="Hint" isOpen={isOpen} handleClose={handleClose}>
-      <div className="inline-block transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all dark:bg-gray-800 sm:my-8 sm:w-full sm:max-w-md sm:p-6 sm:align-middle">
+      <div className="transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all dark:bg-gray-800 sm:my-8 sm:w-full sm:max-w-md sm:p-6 sm:align-middle">
+        <p className="dark:text-white">{verseText}</p>
+        <a
+          href={referenceUrl}
+          className="place-content-center text-center text-black dark:text-white dark:hover:text-neutral-500"
+        >
+          bible.com
+        </a>
         <button
           onClick={() => handleClose()}
           tabIndex={0}
           aria-pressed="false"
           className="absolute right-4 top-4"
         ></button>
-      </div>
-      <div className="mt-2 flex flex-col divide-y">
-        <p>{verseText}</p>
-        <a
-          href={referenceUrl}
-          className="text-center text-black dark:text-white"
-        >
-          bible.com
-        </a>
       </div>
     </BaseModal>
   )
