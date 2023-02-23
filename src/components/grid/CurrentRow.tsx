a2977010b092ef9539292a6477dcb4e7fa448fcf
kadenbking <kadenbking@gmail.com>
2023-02-22 22:48:44 -0600

    added cell classes based on solution size

--- src/components/grid/CurrentRow.tsx
+++ src/components/grid/CurrentRow.tsx
@@ -4,9 +4,10 @@ import { Cell } from './Cell'
 type Props = {
   guess: string
   className: string
+  solutionLength: number
 }
 
-export const CurrentRow = ({ guess, className }: Props) => {
+export const CurrentRow = ({ guess, className, solutionLength }: Props) => {
   const splitGuess = unicodeSplit(guess)
   const emptyCells = Array.from(Array(solution.length - splitGuess.length))
   const classes = `flex justify-center mb-1 ${className}`
@@ -14,10 +15,10 @@ export const CurrentRow = ({ guess, className }: Props) => {
   return (
     <div className={classes}>
       {splitGuess.map((letter, i) => (
-        <Cell key={i} value={letter} />
+        <Cell key={i} value={letter} solutionLength={solutionLength} />
       ))}
       {emptyCells.map((_, i) => (
-        <Cell key={i} />
+        <Cell key={i} solutionLength={solutionLength} />
       ))}
     </div>
   )
--- src/components/grid/EmptyRow.tsx
+++ src/components/grid/EmptyRow.tsx
@@ -1,13 +1,17 @@
 import { solution } from '../../lib/words'
 import { Cell } from './Cell'
 
-export const EmptyRow = () => {
+type Props = {
+  solutionLength: number
+}
+
+export const EmptyRow = ({ solutionLength }: Props) => {
   const emptyCells = Array.from(Array(solution.length))
 
   return (
     <div className="mb-1 flex justify-center">
       {emptyCells.map((_, i) => (
-        <Cell key={i} />
+        <Cell key={i} solutionLength={solutionLength} />
       ))}
     </div>
   )
