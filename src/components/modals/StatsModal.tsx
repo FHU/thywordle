c816ab49e865e7b181f1719e3243a40e74b9c25e
djyarnell0 <djyarnell@hotmail.com>
2023-02-26 17:57:23 -0600

    Fixed button to send to bible.com

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -147,7 +147,7 @@ export const StatsModal = ({
       <hr className="mt-4 -mb-4 border-gray-500" />
       <div className="mt-5 columns-2 items-center items-stretch justify-center text-center dark:text-white sm:mt-6">
         <div className="mt-3 text-xs">{VIEW_VERSE_TEXT}</div>
-        <a>
+        <a href={referenceUrl}>
           <button
             type="button"
             className="mt-2 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-center text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-base"
