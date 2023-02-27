41e335ec84e83de82671c54e6dd6c4b029b5fd92
Kenan Casey <kenancasey@gmail.com>
2023-02-27 16:30:47 -0600

    Update alt on quote

--- src/components/modals/HintModal.tsx
+++ src/components/modals/HintModal.tsx
@@ -12,7 +12,7 @@ export const HintModal = ({ isOpen, handleClose, verseText }: Props) => {
     <BaseModal title="Hint" isOpen={isOpen} handleClose={handleClose}>
       <img
         src={openQuote}
-        alt="OpenQuote.png"
+        alt=""
         className="absolute left-4 top-8 w-10 md:top-16"
       />
       <div className="transform overflow-hidden px-4 pt-5 pb-4 text-left align-bottom transition-all sm:my-8 sm:w-full sm:max-w-md sm:p-6 sm:align-middle">
