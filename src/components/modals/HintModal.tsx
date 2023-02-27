62af5eae5dc8e9d79ed1655e87cb9b0a211a26e6
Kenan Casey <kenancasey@gmail.com>
2023-02-27 15:52:54 -0600

    Fix hint quote on mobile

--- src/components/modals/HintModal.tsx
+++ src/components/modals/HintModal.tsx
@@ -14,10 +14,10 @@ export const HintModal = ({ isOpen, handleClose, verseText }: Props) => {
       <img
         src={openQuote}
         alt="OpenQuote.png"
-        className="absolute top-16 left-5 w-10"
+        className="absolute left-4 top-8 w-10 md:top-16"
       />
       <div className="transform overflow-hidden px-4 pt-5 pb-4 text-left align-bottom transition-all sm:my-8 sm:w-full sm:max-w-md sm:p-6 sm:align-middle">
-        <p className=" text-center dark:text-white">{verseText}</p>
+        <p className="text-center dark:text-white">{verseText}</p>
 
         <button
           onClick={() => handleClose()}
