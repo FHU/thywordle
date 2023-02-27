b5d029261451d4b1ec640cfc57011a6bc9136bc2
kadenbking <kadenbking@gmail.com>
2023-02-26 19:40:31 -0600

    added conditional display

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -144,20 +144,22 @@ export const StatsModal = ({
           <MigrationIntro handleMigrateStatsButton={handleMigrateStatsButton} />
         </div>
       )}
-      <hr className="mt-4 -mb-4 border-gray-500" />
       {(isGameLost || isGameWon) && (
-        <div className="mt-5 columns-2 items-center items-stretch justify-center text-center dark:text-white sm:mt-6">
-          <div className="mt-3 text-xs">{VIEW_VERSE_TEXT}</div>
-          <a href={referenceUrl} target="_blank" rel="noreferrer">
-            <button
-              type="button"
-              className="mt-2 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-center text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-base"
-            >
-              <BookOpenIcon className="mr-2 h-6 w-6 cursor-pointer dark:stroke-white" />
-              {VERSE_TEXT_BUTTON}
-            </button>
-          </a>
-        </div>
+        <>
+          <hr className="mt-4 -mb-4 border-gray-500" />
+          <div className="mt-5 columns-2 items-center items-stretch justify-center text-center dark:text-white sm:mt-6">
+            <div className="mt-3 text-xs">{VIEW_VERSE_TEXT}</div>
+            <a href={referenceUrl} target="_blank" rel="noreferrer">
+              <button
+                type="button"
+                className="mt-2 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-center text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-base"
+              >
+                <BookOpenIcon className="mr-2 h-6 w-6 cursor-pointer dark:stroke-white" />
+                {VERSE_TEXT_BUTTON}
+              </button>
+            </a>
+          </div>
+        </>
       )}
     </BaseModal>
   )
