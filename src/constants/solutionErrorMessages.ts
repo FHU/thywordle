793a9cdf2ef2935ca81d47335a8c096d70996eb8
kadenbking <kadenbking@gmail.com>
2023-03-20 14:20:53 -0500

    added more error messages

--- src/constants/solutionErrorMessages.ts
+++ src/constants/solutionErrorMessages.ts
@@ -1,3 +1,8 @@
 export const solutionErrorMessages: { [key: string]: string } = {
-  PSALMS: 'Did you mean Psalm?',
+  PSALMS: 'Try Psalm',
+  'OBADIAH1:': 'Remove the chapter number and colon',
+  'PHILEMON1:': 'Remove the chapter number and colon',
+  '2JOHN1:': 'Remove the chapter number and colon',
+  '3JOHN1:': 'Remove the chapter number and colon',
+  'JUDE1:': 'Remove the chapter number and colon',
 }
