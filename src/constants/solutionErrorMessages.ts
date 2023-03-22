33fe8817c38f98a4bb6369c583bfe2ae41f8f006
Kaden King <70766937+kadenbking@users.noreply.github.com>
2023-03-22 14:54:36 -0500

    THY-57 custom error messages (#39)
    * fix new solution message
    
    * add custom error message dictionary
    
    * update new game string
    
    * added more error messages
    
    * Add quotes to error message
    
    ---------
    
    Co-authored-by: Kenan Casey <kenancasey@gmail.com>

---
+++ src/constants/solutionErrorMessages.ts
@@ -0,0 +1,8 @@
+export const solutionErrorMessages: { [key: string]: string } = {
+  PSALMS: 'Try "Psalm"',
+  'OBADIAH1:': 'Remove the chapter number and colon',
+  'PHILEMON1:': 'Remove the chapter number and colon',
+  '2JOHN1:': 'Remove the chapter number and colon',
+  '3JOHN1:': 'Remove the chapter number and colon',
+  'JUDE1:': 'Remove the chapter number and colon',
+}
