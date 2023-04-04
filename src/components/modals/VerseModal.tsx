5093bebaa6f460c58f288f959e7dfe7c83319bc7
kadenbking <kadenbking@gmail.com>
2023-04-04 15:35:51 -0500

    add custom copied text message

--- src/components/modals/VerseModal.tsx
+++ src/components/modals/VerseModal.tsx
@@ -8,6 +8,8 @@ type Props = {
 }
 
 export const VerseModal = ({ isOpen, handleClose, verseText }: Props) => {
+  const copiedTextMessage = `Thou shalt not steal or attempt to cheat at ThyWordle.com`
+
   return (
     <BaseModal title="Verse" isOpen={isOpen} handleClose={handleClose}>
       <img
@@ -15,7 +17,13 @@ export const VerseModal = ({ isOpen, handleClose, verseText }: Props) => {
         alt=""
         className="absolute left-4 top-8 w-10 md:top-16"
       />
-      <div className="transform overflow-hidden px-4 pt-5 pb-4 text-left align-bottom transition-all sm:my-8 sm:w-full sm:max-w-md sm:p-6 sm:align-middle">
+      <div
+        className="transform overflow-hidden px-4 pt-5 pb-4 text-left align-bottom transition-all sm:my-8 sm:w-full sm:max-w-md sm:p-6 sm:align-middle"
+        onCopy={(e) => {
+          e.preventDefault()
+          navigator.clipboard.writeText(copiedTextMessage)
+        }}
+      >
         <p className="text-center dark:text-white">{verseText}</p>
 
         <button
