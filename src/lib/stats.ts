0e9ccf75be1c150ea3f6b9ed79007e5a75331907
Kenan Casey <kenancasey@gmail.com>
2023-04-12 14:54:21 -0500

    Add score and avgNumGuesses to GameStats
    Add function to compute average number of guesses and call function when stats are computed.

--- src/lib/stats.ts
+++ src/lib/stats.ts
@@ -30,6 +30,7 @@ export const addStatsForCompletedGame = (
   }
 
   stats.successRate = getSuccessRate(stats)
+  stats.avgNumGuesses = getAverageNumberGuesses(stats)
 
   saveStatsToLocalStorage(stats)
   return stats
@@ -42,6 +43,8 @@ const defaultStats: GameStats = {
   bestStreak: 0,
   totalGames: 0,
   successRate: 0,
+  score: 0,
+  avgNumGuesses: 0,
 }
 
 export const loadStats = () => {
@@ -54,4 +57,16 @@ const getSuccessRate = (gameStats: GameStats) => {
   return Math.round(
     (100 * (totalGames - gamesFailed)) / Math.max(totalGames, 1)
   )
+}
+
+const getAverageNumberGuesses = (gameStats: GameStats) => {
+  const { winDistribution } = gameStats
+  let totalGuesses = 0
+  let totalGames = 0
+  for (let i = 0; i < winDistribution.length; i++) {
+    totalGames += winDistribution[i]
+    totalGuesses += winDistribution[i] * (i + 1)
+  }
+
+  return Number((totalGuesses / totalGames).toFixed(2))
 }
