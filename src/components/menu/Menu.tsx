86f82898b9c2699c10b269aea329a9b4640b024b
Kenan Casey <kenancasey@gmail.com>
2023-12-21 21:30:04 -0600

    Cleanup Menu

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,3 +1,12 @@
+import {
+  ChevronDoubleRightIcon,
+  GlobeIcon,
+  InformationCircleIcon,
+  MailIcon,
+  QuestionMarkCircleIcon,
+  UserGroupIcon,
+  UserIcon,
+} from '@heroicons/react/outline'
 import React from 'react'
 import { useDetectClickOutside } from 'react-detect-click-outside'
 import { Link } from 'react-router-dom'
@@ -24,7 +33,7 @@ export const Menu = ({ showMenu, setShowMenu }: props) => {
     <React.Fragment>
       <div
         ref={ref}
-        className={`fixed left-0 top-0 z-10 h-full w-3/4 bg-slate-400 p-8 text-white duration-300 ease-in-out dark:bg-slate-700 md:w-1/2 md:w-1/3 md:pl-8 ${
+        className={`fixed left-0 top-0 z-10 h-full w-3/4 bg-indigo-100 p-8 text-white duration-300 ease-in-out dark:bg-slate-700 md:w-1/3 md:pl-8 ${
           showMenu ? 'translate-x-0 ' : '-translate-x-full'
         }`}
       >
@@ -36,35 +45,10 @@ export const Menu = ({ showMenu, setShowMenu }: props) => {
             setShowMenu(false)
           }}
         >
+          <ChevronDoubleRightIcon className="mr-2 h-6 w-6 cursor-pointer dark:stroke-white" />{' '}
           Play
         </Link>
-        <Link
-          to="/about"
-          className={`mt-8 ${menuItemClasses}`}
-          onClick={() => {
-            setShowMenu(false)
-          }}
-        >
-          About
-        </Link>
-        <Link
-          to="/help"
-          className={`mt-8 ${menuItemClasses}`}
-          onClick={() => {
-            setShowMenu(false)
-          }}
-        >
-          Help
-        </Link>
-        <Link
-          to="/profile"
-          className={`mt-8 ${menuItemClasses}`}
-          onClick={() => {
-            setShowMenu(false)
-          }}
-        >
-          Profile
-        </Link>
+
         <Link
           to="/leaderboard"
           className={`mt-8 ${menuItemClasses}`}
@@ -72,6 +56,7 @@ export const Menu = ({ showMenu, setShowMenu }: props) => {
             setShowMenu(false)
           }}
         >
+          <GlobeIcon className="mr-2 h-6 w-6 cursor-pointer dark:stroke-white" />{' '}
           Leaderboard
         </Link>
         <Link
@@ -81,33 +66,56 @@ export const Menu = ({ showMenu, setShowMenu }: props) => {
             setShowMenu(false)
           }}
         >
+          <UserGroupIcon className="mr-2 h-6 w-6 cursor-pointer dark:stroke-white" />{' '}
           Groups
         </Link>
+
         <Link
-          to="mailto:thywordle@gmail.com"
-          target="_blank"
-          className={
-            menuItemClasses +
-            ' absolute bottom-0 right-0 px-8 py-16 md:py-8 md:text-lg'
-          }
+          to="/profile"
+          className={`mt-8 ${menuItemClasses}`}
           onClick={() => {
             setShowMenu(false)
           }}
         >
-          Send Feedback
+          <UserIcon className="mr-2 h-6 w-6 cursor-pointer dark:stroke-white" />{' '}
+          Profile
         </Link>
+
+        <div className="mt-16  opacity-90">
+          <Link
+            to="/about"
+            className={`mt-8 ${menuItemClasses} text-base font-normal`}
+            onClick={() => {
+              setShowMenu(false)
+            }}
+          >
+            <InformationCircleIcon className="mr-2 h-6 w-6 cursor-pointer dark:stroke-white" />{' '}
+            About
+          </Link>
+          <Link
+            to="/help"
+            className={`mt-8 ${menuItemClasses} text-base font-normal`}
+            onClick={() => {
+              setShowMenu(false)
+            }}
+          >
+            <QuestionMarkCircleIcon className="mr-2 h-6 w-6 cursor-pointer dark:stroke-white" />{' '}
+            Help
+          </Link>
+        </div>
 
         <Link
           to="mailto:thywordle@gmail.com"
           target="_blank"
           className={
             menuItemClasses +
-            ' absolute bottom-0 right-0 py-16 px-8 md:py-8 md:text-lg'
+            ' absolute bottom-0 right-0 px-8 py-8 font-normal md:text-sm'
           }
           onClick={() => {
             setShowMenu(false)
           }}
         >
+          <MailIcon className="mr-2 mt-0 h-4 w-4 cursor-pointer  dark:stroke-white" />{' '}
           Send Feedback
         </Link>
       </div>
