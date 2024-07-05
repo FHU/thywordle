5c731aaf6eb60b578737ce2080843e0eb32ac8e1
Kaden King <70766937+kadenbking@users.noreply.github.com>
2024-07-05 09:44:15 -0500

    move config

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyBCp5wNSfqPfO9Od3wDdcJiZvf0h0w9Bkk',
+  authDomain: 'thywordle-93fd5.firebaseapp.com',
+  projectId: 'thywordle-93fd5',
+  storageBucket: 'thywordle-93fd5.appspot.com',
+  messagingSenderId: '779774332038',
+  appId: '1:779774332038:web:d1c406b9c4da69679c1507',
+  measurementId: 'G-4WR7Z6S51M',
 }
 
 const app = initializeApp(firebaseConfig)
